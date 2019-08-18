05be6ae2c59d19919d05ce53d092d30f011bc4ef
vishalraj258 <vishalraj258@gmail.com>
2019-08-18 22:55:38 +0530

    Updates

---
+++ precache-manifest.c79f92fdb1a7ad8f3343507609a704a3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0031406b20f0992f9495c3b58a29a6f4",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "c3938b1e78919d8eb305",
+    "url": "/monster-rolodex/static/css/main.c5fc0e6c.chunk.css"
+  },
+  {
+    "revision": "2ab373573a2da60aeefa",
+    "url": "/monster-rolodex/static/js/2.b3f2c5c4.chunk.js"
+  },
+  {
+    "revision": "c3938b1e78919d8eb305",
+    "url": "/monster-rolodex/static/js/main.475ffab2.chunk.js"
+  },
+  {
+    "revision": "18ed1ec8b444b237e95a",
+    "url": "/monster-rolodex/static/js/runtime~main.ff9a5c26.js"
+  }
+]);
